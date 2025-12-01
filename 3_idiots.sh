f8cd2d2159cad43f9a5c6e7939c09abc31e2d5d6
Shashank <randhawaguru892@gmail.com>
2025-12-01 22:45:04 +0530

    added 3_idiots.sh script

---
+++ 3_idiots.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+hero="rancho"
+villain="virus"
+
+echo "3 idiots ka hero hai $hero"
+
+echo "3 idiots ka villain hai $villain"
+
+echo "Current logged in user: $USER"
+
+# read -p "Pura naam batao: " puranaam
+# echo "$puranaam"
+
+echo "movie ka naam: $0"
+
+echo "first idiot: $1"
+
+echo "second idiot: $2"
+
+echo "third idiot: $3"
+
+echo "total $# idiots $@"
+
+
